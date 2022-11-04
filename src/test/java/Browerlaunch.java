24ab29b383a478657c5b47b54f66260f82cbf97a
Jaffarjas <callmejaffar@gmail.com>
2022-11-04 07:06:37 +0530

    project structure

---
+++ src/test/java/Browerlaunch.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browerlaunch {
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		WebElement user = driver.findElement(By.id("email"));
+		user.sendKeys("jaffar");
+	}
+
+}
